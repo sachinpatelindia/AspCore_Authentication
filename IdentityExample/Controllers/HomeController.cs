f16bea84bcc3ef65fe0e47e807532d60b17f5822
Sachin Patel <sachin@skpatel.net>
2020-02-11 10:54:15 +0530

    email verifacation added, email tested on PAPERCUT

--- IdentityExample/Controllers/HomeController.cs
+++ IdentityExample/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 
 namespace IdentityExample.Controllers
 {
@@ -14,10 +15,13 @@ namespace IdentityExample.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        private readonly IEmailService _emailService;
+
+        public HomeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager,IEmailService emailService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailService = emailService;
         }
         public IActionResult Index()
         {
@@ -67,6 +71,10 @@ namespace IdentityExample.Controllers
             var result = await _userManager.CreateAsync(user, password);
             if (result.Succeeded)
             {
+                //generat email token
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var link = Url.Action(nameof(VeryfyEmail),"Home",new {userId=user.Id,code },Request.Scheme,Request.Host.ToString());
+                await _emailService.SendAsync("sachin@skpatel.net", "email verification", link);// $"<a href=\"{link}\">Veryfy emil</a>");
                 var signInresult = await _signInManager.PasswordSignInAsync(user, password, false, false);
                 if (signInresult.Succeeded)
                 {
@@ -75,6 +83,17 @@ namespace IdentityExample.Controllers
             }
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> VeryfyEmail(string userId, string code)
+        {
+            var user =await _userManager.FindByIdAsync(userId);
+            if (user == null) return BadRequest();
+            var result= await _userManager.ConfirmEmailAsync(user,code);
+            if (result.Succeeded) return View();
+            return BadRequest();
+        }
+
+        public IActionResult EmailVerification() => View();
         public async Task<IActionResult> SignOut()
         {
            await _signInManager.SignOutAsync();
--- IdentityExample/Startup.cs
+++ IdentityExample/Startup.cs
@@ -8,37 +8,46 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 
 namespace IdentityExample
 {
     public class Startup
     {
+        IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(cfg=>cfg.UseInMemoryDatabase("Memory"));
+            services.AddDbContext<AppDbContext>(cfg => cfg.UseInMemoryDatabase("Memory"));
 
-            services.AddIdentity<IdentityUser, IdentityRole>(cfg=> {
+            services.AddIdentity<IdentityUser, IdentityRole>(cfg =>
+            {
                 cfg.Password.RequireDigit = false;
                 cfg.Password.RequiredLength = 4;
                 cfg.Password.RequireNonAlphanumeric = false;
                 cfg.Password.RequireUppercase = false;
-            
-            })
+                cfg.SignIn.RequireConfirmedEmail = true;
 
+            })
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.ConfigureApplicationCookie(cfg=>
+            services.ConfigureApplicationCookie(cfg =>
             {
                 cfg.Cookie.Name = "id.cookie";
                 cfg.LoginPath = "/home/login";
             });
-    
 
+            services.AddMailKit(config => config.UseMailKit(_config.GetSection("Email").Get<MailKitOptions>()));
             services.AddControllersWithViews();
         }
 
